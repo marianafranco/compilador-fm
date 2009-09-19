150804b0ed5a1550c934033c5f10478308caa6dc
felipe.yoshida <felipe.yoshida@aaa21bac-a22c-11de-b161-9f508301ba8e>
2009-09-19 19:00:52 +0000

    Versao dividida

---
+++ trunk/simulador_AFD.c
@@ -0,0 +1,31 @@
+#include "simulador_AFD.h"
+
+void simula_AFD (AFD *automato, FILE *cod_fonte, fluxo_tokens *tokens, tabela_simbolos *tabela) {
+	
+	// Número do estado atual
+	int estado_atual = 0;
+	// Buffer do ultimo caracter lido
+	char buffer;
+	
+	// Coloca caracter no buffer
+	buffer = fgetc(cod_fonte);
+	
+	// Enquanto não chegarmos ao fim do arquivo
+	while (buffer != EOF) {
+		// Caso tivermos um espaço ou uma quebra de linha
+		while (0) {
+			// Próximo caracter
+			buffer = fgetc(cod_fonte);
+		}
+		
+		// Muda de estado
+		
+		// Caso o token for completado com sucesso
+		if (0) {
+			// Grava token
+		}
+		
+		// Próximo caracter
+		buffer = fgetc(cod_fonte);
+	}
+}
---
+++ trunk/simulador_AFD.h
@@ -0,0 +1,4 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void simula_AFD (AFD *automato, FILE *cod_fonte, fluxo_tokens *tokens, tabela_simbolos *tabela);
