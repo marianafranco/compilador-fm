4c50a5fb2c3290fba61321bfa9944e661f9a448d
ramos.franco <ramos.franco@aaa21bac-a22c-11de-b161-9f508301ba8e>
2009-09-20 17:26:53 +0000

    Adicionado o atributo numEstados e criado o método adiciona(estado, indice).

--- trunk/CompiladorFM/src/compilador/lexico/estruturas/AFD.java
+++ trunk/CompiladorFM/src/compilador/lexico/estruturas/AFD.java
@@ -5,18 +5,32 @@ import compilador.lexico.estruturas.Estado;
 public class AFD {
 
 	private int estadoAtivo;
+	private int numEstados;
 	private Estado estados[];
 	
 	public AFD () {
 		this.estadoAtivo = -1;
 	}
 	
-	public void adicionaEstado (Estado adicionado, boolean inicial) {
+	public void adicionaEstado (Estado adicionado, boolean inicial, int indice) {
 		if (inicial == true) {
 			this.estadoAtivo = adicionado.getId();
 		}
-		this.estados[this.estados.length] = adicionado;
+		this.estados[indice] = adicionado;
 	}
+
+	public void adicionaEstado (Estado adicionado, int indice) {
+		if (adicionado.getId() == 0) {
+			this.estadoAtivo = adicionado.getId();
+		}
+		this.estados[indice] = adicionado;
+	}
+	
+	public void setNumEstados(int numEstados){
+		this.numEstados = numEstados;
+		this.estados = new Estado [this.numEstados];
+	}
+	
 	
 	public void recebeEntrada (char atual, char proximo) {
 		
